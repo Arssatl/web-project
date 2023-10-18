f6b79892a39b8e392fcc0e8cc5ab353897ec7b50
Arssatl <satlykov.ars@gmail.com>
2023-10-18 22:19:51 +0500

    checkbox

--- WEB/ToDo-List/script.js
+++ WEB/ToDo-List/script.js
@@ -18,7 +18,7 @@ const label = document.querySelector(".label")
 checkboxes = document.querySelectorAll(".checkbox")
 tasks = document.querySelectorAll("li")
 
-const taskList = ["Task 1"]
+const taskList = []
 
 checkboxClick = function() {
     // if (checkbox.checked) {
@@ -31,9 +31,17 @@ checkboxClick = function() {
     tasks.forEach(function(e){
         if (e.childNodes[1].childNodes[1].checked) {
             e.childNodes[1].childNodes[3].style.textDecoration = "line-through"
+            id = e.childNodes[1].childNodes[1].id
+            task = JSON.parse(localStorage.getItem(id))
+            task.check = true
+            localStorage.setItem(id, JSON.stringify(task))
         }
         else {
             e.childNodes[1].childNodes[3].style.textDecoration = "none"
+            id = e.childNodes[1].childNodes[1].id
+            task = JSON.parse(localStorage.getItem(id))
+            task.check = false
+            localStorage.setItem(id, JSON.stringify(task))
         }
         // if (e.childNodes[1].childNodes[3].style.textDecoration = "line-through") {
         //     e.childNodes[1].childNodes[1].checked = true
@@ -42,7 +50,7 @@ checkboxClick = function() {
         //     e.childNodes[1].childNodes[1].checked = false
         // }
     })
-
+    console.log(tasks) 
 }
 
 formButtonClick = function() {
@@ -52,10 +60,10 @@ formButtonClick = function() {
     }
     ulList.innerHTML += 
     `
-        <li id="${taskList.length}">
+        <li id="${localStorage.length}">
             <div class="task">
-                <input type="checkbox" id="task-${taskList.length}" class="checkbox">
-                <label class="label" for="task-${taskList.length}">${formInputText}</label>
+                <input type="checkbox" id="${localStorage.length}" class="checkbox">
+                <label class="label" for="${localStorage.length}">${formInputText}</label>
             </div>
             <div class="buttons">
                 <img src="icons/edit.png" class="edit">
@@ -67,13 +75,90 @@ formButtonClick = function() {
 
     checkboxes = document.querySelectorAll(".checkbox")
     checkboxes.forEach(function(e) {
+        e.addEventListener("change", checkboxClick)
+    });
+
+    tasks = document.querySelectorAll("li")
+    tasks.forEach(function(e){
+        if (e.childNodes[1].childNodes[3].style.textDecoration == "line-through") {
+            e.childNodes[1].childNodes[1].checked = true
+        }
+        else {
+            e.childNodes[1].childNodes[1].checked = false
+        }
+    })
+
+    checkboxes = document.querySelectorAll(".checkbox")
+    checkboxes.forEach(function(e) {
         e.addEventListener("click", checkboxClick)
     });
 
     console.log(checkboxes)
+
+    localStorage.setItem(localStorage.length, JSON.stringify({id: localStorage.length, check: false, label: formInputText}))
 }
+
+const list = document.querySelector(".list")
+for (i = 0; i < localStorage.length; i++) {
+    task = JSON.parse(localStorage.getItem(localStorage.key(i)))
+    if (task.check) {
+        checked = "checked"
+    }
+    else {
+        checked = ""
+    }
+    list.innerHTML += `
+    <li id="${task.id}">
+        <div class="task">
+            <input type="checkbox" id="task-${task.id}" class="checkbox" ${checked}>
+            <label for="task-${task.id}" class="label">${task.label}</label>
+        </div>
+        <div class="buttons">
+            <img src="icons/edit.png" class="edit">
+            <img src="icons/delete.png" class="delete">
+        </div>
+    </li>
+    `
+}
+
+tasks = document.querySelectorAll("li")
+tasks.forEach(function(e){
+    if (e.childNodes[1].childNodes[1].checked) {
+        e.childNodes[1].childNodes[3].style.textDecoration = "line-through"
+    }
+    else {
+        e.childNodes[1].childNodes[3].style.textDecoration = "none"
+    }
+})
 
 formButton.addEventListener("click", formButtonClick)
 checkboxes.forEach(function(e) {
     e.addEventListener("click", checkboxClick)
 });
+
+// task = "aaa"
+// console.log(localStorage.getItem("task 1"))
+// localStorage.setItem("task 1", task)
+// console.log(localStorage.getItem("task 1"))
+// localStorage.removeItem("task 1")
+// tl = ["a", "b", "c"]
+// localStorage.setItem("TaskList", JSON.stringify(tl))
+// console.log(tl)
+// console.log(JSON.parse(localStorage.getItem("TaskList")))
+
+//localStorage.clear()
+
+/* 
+<li id="${taskList.length}">
+    <div class="task">
+        <input type="checkbox" id="task-${taskList.length}" class="checkbox">
+        <label class="label" for="task-${taskList.length}">${formInputText}</label>
+    </div>
+    <div class="buttons">
+        <img src="icons/edit.png" class="edit">
+        <img src="icons/delete.png" class="delete">
+    </div>
+</li> 
+*/
+
+
